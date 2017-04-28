6fafa6ec58661fc8c1b2bf4fe14c8ac1da3c9663
Nekosuki <nanorarias@gmail.com>
2017-04-28 16:25:31 -0300

    Había dos main(); saqué uno y cambié un par de cosas más.

--- src/main/java/tp1/App.java
+++ src/main/java/tp1/App.java
@@ -4,18 +4,19 @@ import org.uqbar.arena.Application;
 import org.uqbar.arena.windows.Window;
 
 import tp1.model.Metric;
+import tp1.view.MainView;
 
 public class App extends Application {
 
 	@Override
 	protected Window<?> createMainWindow() {
-		return null;
+		return new MainView(this);
 	}
 	
 	public static void main(String[] args) {
 		Metric.loadMetrics();
 		Metric.getMetrics().stream().forEach(metric -> System.out.println(metric));
 		
-//		new App().start();
+		new App().start();
 	}
 }
---
+++ src/main/java/tp1/viewModel/MainViewModel.java
@@ -0,0 +1,10 @@
+package tp1.viewModel;
+
+import org.uqbar.commons.utils.Observable;
+
+@Observable
+public class MainViewModel {
+	
+	
+
+}
