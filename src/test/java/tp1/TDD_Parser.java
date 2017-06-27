44ec26c9f58a9ee43ea8eb0553a2016c3bfe8a3a
FedericoVigi <federico9261@gmail.com>
2017-06-27 19:18:54 -0300

    Nuevo parser: suma y resta binaria

---
+++ src/test/java/tp1/TDD_Parser.java
@@ -0,0 +1,54 @@
+package tp1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TDD_Parser {
+
+	public double evaluar(String formula){
+		int i;
+		String termino = "";
+		for (i=0; i< formula.length(); i++){ //recorro el string
+			char caracter = formula.charAt(i);
+			 switch (caracter) {
+			 	case '+': 
+	            return	Double.parseDouble(termino) + evaluar(formula.substring(i+1));
+	            case '-':
+	            return Double.parseDouble(termino) - evaluar(formula.substring(i+1));
+//	            case '*':
+//	            case '/':
+	            default: 
+	            termino = termino + caracter; 
+	            break;
+	         }
+	      }
+		
+		return Double.parseDouble(termino); //para los casos donde solo hay un numero ej, formula = "7"
+	}
+	
+	@Test
+	public void SieteEsSiete() {
+		String formula =  "7";
+		Double resultado = evaluar(formula);
+		assertEquals(7, resultado, 0.0);
+		
+	}
+	
+	
+	@Test
+	public void dosYDosSonCuatroTest() {
+		String formula =  "2+2";
+		Double resultado = evaluar(formula);
+		assertEquals(4, resultado, 0.0);
+		
+	}
+	
+	@Test
+	public void ochoMenosUnoSiete() {
+		String formula =  "8-1";
+		Double resultado = evaluar(formula);
+		assertEquals(7, resultado, 0.0);
+	}
+	
+}
