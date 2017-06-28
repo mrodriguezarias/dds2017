a2f845df32fdb60e13a742bbe7d2cd4e3ca5f5ab
FedericoVigi <federico9261@gmail.com>
2017-06-28 09:25:25 -0300

    Parser: se pueden evaluar expresiones numericas con parentesis simples

--- src/test/java/tp1/TDD_Parser.java
+++ src/test/java/tp1/TDD_Parser.java
@@ -7,51 +7,62 @@ import org.junit.Test;
 public class TDD_Parser {
 
 	public double evaluar(String formula){
+		/*-- Este metodo evaula una formula sin errores, es decir que se debe chequear antes por errores --*/
 		int i;
 		String termino = "";
+		String subtermino = ""; //un termino dentro de un parentesis
 		for (i=0; i< formula.length(); i++){ //recorro el string para separar terminos por + y -
 			char caracter = formula.charAt(i);
-			 switch (caracter) {
-			 	case ' ':
-			 		break; //ignora espacios
-			 	case '+': 
-			 		return evaluar(termino) + evaluar(formula.substring(i+1));
-	            case '-':
-	            	return evaluar(termino) - evaluar(formula.substring(i+1));
-	            default: 
-		            termino = termino + caracter; 
-		            break;
-	        }
+			switch (caracter) {
+			case ' ':
+				break; //ignora espacios
+			case '+': 
+				return evaluar(termino) + evaluar(formula.substring(i+1));
+			case '-':
+				return evaluar(termino) - evaluar(formula.substring(i+1));
+			case '(':
+				while((caracter = formula.charAt(++i))!=')'){ subtermino += caracter;}
+				termino += String.valueOf(evaluar(subtermino));
+				break;
+				
+			default: 
+				termino += caracter; 
+				break;
+			}
 	      }
 		
-		/*-- Aca se supone que llega una formula solo si no tiene sumas ni restas --*/		
+		/*-- A este punto llega una formula solo si no tiene sumas ni restas --*/	
+		
 		termino = ""; //reseteo el "termino" para recorrer el string nuevamente
 
 		for (i=0; i< formula.length(); i++){
 			char caracter = formula.charAt(i);
 			switch (caracter) {
-            case ' ':
-            	break;
+			case ' ':
+				break;
 			case '/': 
 				return evaluar(termino) / evaluar(formula.substring(i+1));
-            case '*':
-            	return evaluar(termino) * evaluar(formula.substring(i+1));
+			case '*':
+				return evaluar(termino) * evaluar(formula.substring(i+1));
+			case '(':
+				while((caracter = formula.charAt(++i))!=')'){ subtermino += caracter;}
+				termino += String.valueOf(evaluar(subtermino));
+				break;
 			default: 
-	            termino += caracter; 
-	            break;
+				termino += caracter; 
+				break;
 			}
 			 
 		}
 		
-		return Double.parseDouble(termino); //para los casos donde solo hay un numero ej, formula = "7"
+		return Double.parseDouble(termino); //para los casos donde solo hay un numero ej: formula = "7"
 	}
 	
 	@Test
-	public void SieteEsSiete() {
+	public void sieteEsSiete() {
 		String formula =  "7";
 		Double resultado = evaluar(formula);
-		assertEquals(7, resultado, 0.0);
-		
+		assertEquals(7, resultado, 0.0);	
 	}
 	
 	@Test
@@ -70,7 +81,7 @@ public class TDD_Parser {
 	}
 	
 	@Test
-	public void TresMasTresMasTres() {
+	public void tresMasTresMasTres() {
 		String formula =  "3+3+3";
 		Double resultado = evaluar(formula);
 		assertEquals(9, resultado, 0.0);
@@ -84,24 +95,42 @@ public class TDD_Parser {
 	}
 	
 	@Test
-	public void TresDividoTresIgualUno() {
+	public void tresDividoTresIgualUno() {
 		String formula =  "3/3";
 		Double resultado = evaluar(formula);
 		assertEquals(1, resultado, 0.0);
 	}
 	
 	@Test
-	public void DosMasTresPorCuatroIgualCatorce() {
+	public void dosMasTresPorCuatroIgualCatorce() {
 		String formula =  "2+3*4";
 		Double resultado = evaluar(formula);
 		assertEquals(14, resultado, 0.0);
 	}
 	
 	@Test
-	public void DosMaTresPorCuatroSobreDosMenosCincoIgualNueve() {
+	public void dosMaTresPorCuatroSobreDosMenosCincoIgualNueve() {
 		String formula =  "8+3*4/2-5";
 		Double resultado = evaluar(formula);
 		assertEquals(9, resultado, 0.0);
 	}
+	
+	/* Test que deberia pasar */
+	
+	@Test
+	public void formulaConParentestisSimple() {
+		String formula =  "(8+3*4)/2-5";
+		Double resultado = evaluar(formula);
+		assertEquals(5, resultado, 0.0);
+	}
+	
+	/*-- Test que deberia pasar --*/
+//	@Test
+//	public void formulaConParentestisDoble() {
+//		String formula =  "((8+3)*4)/2-5";
+//		Double resultado = evaluar(formula);
+//		assertEquals(17, resultado, 0.0);
+//	}
+	
 	
 }
