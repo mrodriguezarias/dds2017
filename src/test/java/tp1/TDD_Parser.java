8027ad8a7c5188a8254d87e8f3a2bc23f8ed9bd3
FedericoVigi <federico9261@gmail.com>
2017-06-27 21:00:45 -0300

    Parser: se pueden evaluar formulas numericas con orden de presedenica (+-*/) pero sin parentesis

--- src/test/java/tp1/TDD_Parser.java
+++ src/test/java/tp1/TDD_Parser.java
@@ -9,24 +9,39 @@ public class TDD_Parser {
 	public double evaluar(String formula){
 		int i;
 		String termino = "";
-		for (i=0; i< formula.length(); i++){ //recorro el string
+		for (i=0; i< formula.length(); i++){ //recorro el string para separar terminos por + y -
 			char caracter = formula.charAt(i);
 			 switch (caracter) {
 			 	case ' ':
-			 	break; //ignora espacios
+			 		break; //ignora espacios
 			 	case '+': 
-	            return	Double.parseDouble(termino) + evaluar(formula.substring(i+1));
+			 		return evaluar(termino) + evaluar(formula.substring(i+1));
 	            case '-':
-	            return Double.parseDouble(termino) - evaluar(formula.substring(i+1));
-//	            case '*':
-//	            return	Double.parseDouble(termino) + evaluar(formula.substring(i+1));
-//	            case '/':
-//	            return	Double.parseDouble(termino) + evaluar(formula.substring(i+1));
+	            	return evaluar(termino) - evaluar(formula.substring(i+1));
 	            default: 
-	            termino = termino + caracter; 
-	            break;
-	         }
+		            termino = termino + caracter; 
+		            break;
+	        }
 	      }
+		
+		/*-- Aca se supone que llega una formula solo si no tiene sumas ni restas --*/		
+		termino = ""; //reseteo el "termino" para recorrer el string nuevamente
+
+		for (i=0; i< formula.length(); i++){
+			char caracter = formula.charAt(i);
+			switch (caracter) {
+            case ' ':
+            	break;
+			case '/': 
+				return evaluar(termino) / evaluar(formula.substring(i+1));
+            case '*':
+            	return evaluar(termino) * evaluar(formula.substring(i+1));
+			default: 
+	            termino += caracter; 
+	            break;
+			}
+			 
+		}
 		
 		return Double.parseDouble(termino); //para los casos donde solo hay un numero ej, formula = "7"
 	}
@@ -66,6 +81,27 @@ public class TDD_Parser {
 		String formula =  "4+2-11";
 		Double resultado = evaluar(formula);
 		assertEquals(-5, resultado, 0.0);
+	}
+	
+	@Test
+	public void TresDividoTresIgualUno() {
+		String formula =  "3/3";
+		Double resultado = evaluar(formula);
+		assertEquals(1, resultado, 0.0);
+	}
+	
+	@Test
+	public void DosMasTresPorCuatroIgualCatorce() {
+		String formula =  "2+3*4";
+		Double resultado = evaluar(formula);
+		assertEquals(14, resultado, 0.0);
+	}
+	
+	@Test
+	public void DosMaTresPorCuatroSobreDosMenosCincoIgualNueve() {
+		String formula =  "8+3*4/2-5";
+		Double resultado = evaluar(formula);
+		assertEquals(9, resultado, 0.0);
 	}
 	
 }
